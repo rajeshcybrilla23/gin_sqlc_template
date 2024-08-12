cbb4065f747201c81344866abc70f8165ff17bda
rajeshmanjunath <rajesh@thisisalice.com>
2024-08-12 08:44:38 +0530

    setup test cases crud

---
+++ db/sqlc/account_test.go
@@ -0,0 +1,125 @@
+package db
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/jackc/pgx"
+	"github.com/stretchr/testify/require"
+)
+
+func setUp() {
+	// SETUP METHOD WHICH IS REQUIRED TO RUN FOR EACH TEST METHOD
+	// your code here
+	//https://stackoverflow.com/questions/23729790/how-can-i-do-test-setup-using-the-testing-package-in-go/65428147#65428147
+}
+
+func tearDown(ctx context.Context) {
+	// TEAR DOWN METHOD WHICH IS REQUIRED TO RUN FOR EACH TEST METHOD
+	// your code here
+	testQueries.DeleteAll(context.Background())
+}
+
+var RunTest = CreateForEach(setUp, tearDown)
+
+func createRandomAccount(t *testing.T) Account {
+
+	arg := CreateAccountParams{
+		Owner:    "tom",
+		Balance:  100,
+		Currency: "USD",
+	}
+
+	account, err := testQueries.CreateAccount(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, account)
+
+	require.Equal(t, arg.Owner, account.Owner)
+	require.Equal(t, arg.Balance, account.Balance)
+	require.Equal(t, arg.Currency, account.Currency)
+
+	require.NotZero(t, account.ID)
+	require.NotZero(t, account.CreatedAt)
+
+	return account
+}
+
+func TestCreateAccount(t *testing.T) {
+	RunTest(func() {
+		createRandomAccount(t)
+	})
+}
+
+func TestGetAccount(t *testing.T) {
+	RunTest(func() {
+		account1 := createRandomAccount(t)
+		account2, err := testQueries.GetAccount(context.Background(), account1.ID)
+		require.NoError(t, err)
+		require.NotEmpty(t, account2)
+
+		require.Equal(t, account1.ID, account2.ID)
+		require.Equal(t, account1.Owner, account2.Owner)
+		require.Equal(t, account1.Balance, account2.Balance)
+		require.Equal(t, account1.Currency, account2.Currency)
+		require.WithinDuration(t, account1.CreatedAt, account2.CreatedAt, time.Second)
+	})
+}
+
+func TestUpdateAccount(t *testing.T) {
+	RunTest(func() {
+		account1 := createRandomAccount(t)
+
+		arg := UpdateAccountParams{
+			ID:      account1.ID,
+			Balance: 200,
+		}
+
+		account2, err := testQueries.UpdateAccount(context.Background(), arg)
+		require.NoError(t, err)
+		require.NotEmpty(t, account2)
+
+		require.Equal(t, account1.ID, account2.ID)
+		require.Equal(t, account1.Owner, account2.Owner)
+		require.Equal(t, arg.Balance, account2.Balance)
+		require.Equal(t, account1.Currency, account2.Currency)
+		require.WithinDuration(t, account1.CreatedAt, account2.CreatedAt, time.Second)
+	})
+}
+
+func TestDeleteAccount(t *testing.T) {
+	RunTest(func() {
+		account1 := createRandomAccount(t)
+		err := testQueries.DeleteAccount(context.Background(), account1.ID)
+		require.NoError(t, err)
+
+		account2, err := testQueries.GetAccount(context.Background(), account1.ID)
+		require.Error(t, err)
+		require.EqualError(t, err, pgx.ErrNoRows.Error())
+		require.Empty(t, account2)
+	})
+}
+
+func TestListAccounts(t *testing.T) {
+	RunTest(func() {
+		var lastAccount Account
+		for i := 0; i < 10; i++ {
+			lastAccount = createRandomAccount(t)
+		}
+
+		arg := ListAccountsParams{
+			Owner:  lastAccount.Owner,
+			Limit:  5,
+			Offset: 0,
+		}
+
+		accounts, err := testQueries.ListAccounts(context.Background(), arg)
+		require.NoError(t, err)
+		require.NotEmpty(t, accounts)
+
+		for _, account := range accounts {
+			require.NotEmpty(t, account)
+			require.Equal(t, lastAccount.Owner, account.Owner)
+		}
+	})
+}
---
+++ db/sqlc/main_test.go
@@ -0,0 +1,39 @@
+package db
+
+import (
+	"context"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+var testQueries *Queries
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgres://rajeshmanjunath:password@localhost:5432/gin_sqlc_template?sslmode=disable"
+)
+
+func TestMain(m *testing.M) {
+	connPool, err := pgxpool.New(context.Background(), dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+	testQueries = New(connPool)
+
+	// inform os that connnection was successfulr nad terminate the test case
+	os.Exit(m.Run())
+}
+
+// setup and tear down
+func CreateForEach(setUp func(), tearDown func(ctx context.Context)) func(func()) {
+	ctx := context.Background()
+	return func(testFunc func()) {
+		setUp()
+		testFunc()
+		tearDown(ctx)
+	}
+}
+
